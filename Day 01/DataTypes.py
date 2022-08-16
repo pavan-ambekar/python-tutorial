6172b0b0ee5235aa7d2e26ca370cc2d430ac4e79
Pavan A <pavan479ec@gmail.com>
2022-08-17 00:28:23 +0530

    Data types.

---
+++ Day 01/DataTypes.py
@@ -0,0 +1,81 @@
+# %%
+import math
+name = 'Zoi'
+print(name)
+
+# %%
+age = 22
+print(age)
+
+# %%
+weight = 45.5
+print(weight)
+
+# %%
+skills = ['eating', 'sleeping']
+print(skills[1])
+
+# %%
+features = ('hands', 'legs')
+print(features[-1])
+
+# %%
+# Unordered Collections of Unique Elements
+color = {'white', 'a', 'white', 'black', 'pink'}
+print(color)
+
+# %%
+address = {
+    'city': 'RCR',
+    'State': 'karnataka',
+    'country': 'india'
+}
+print(address)
+address['city'] = 'BAY'
+print(address)
+
+# %%
+x, y, z = 'a', 'b', 'c'
+print(x)
+print(z)
+print(y)
+
+# %%
+a = b = c = d = 'a'
+print(a, b, c, d)
+
+# %%
+cars = ['BMW', 'Audi', 'MB']
+car1, car2, car3 = cars
+print(car1)
+print(car3)
+print(car2)
+# print(car4)
+
+
+# %%
+x = 5
+y = "10"
+print(x + int(y))
+
+# %%
+comx = 3+4j
+
+type(comx)
+
+# %%
+num = 2.5000000000001
+print(round(num))
+
+# %%
+adds = '''HNO 14/5b
+Kannada
+'''
+
+# %%
+name = ['lorem', 'ramu']
+print(len(name[1]))
+
+# %%
+statement = 'donkey in sky'
+print(' ' in statement)
